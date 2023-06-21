1c2df1c728a89fcdc95d94f22d7fbb1c1ec54579
Nathan <nathandaud123@gmail.com>
2023-06-21 19:28:46 +0700

    tambah tabel user_lomba

--- resources/views/tim/index.blade.php
+++ resources/views/tim/index.blade.php
@@ -2,40 +2,43 @@
 <html>
 <head>
     <title>Daftar Tim Lomba</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
-        <h1>List Lomba</h1>
-        @php
-            $tims = \App\Models\Tim::all();
-        @endphp
-        <table class="table table-striped">
-            <thead class="thead-dark">
+        <br>
+        <h1>LombaKuy!</h1>
+        <br>
+        <table class="table">
+            <thead>
                 <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">ID Tim</th>
-                    <th scope="col">Nama Tim</th>
-                    <th scope="col">Action</th>
+                    <th>No</th>
+                    <th>ID Tim</th>
+                    <th>Nama Tim</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                @forelse($tims as $key => $tim)
+                @foreach ($tims as $key => $tim)
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $tim->idTim }}</td>
                     <td>{{ $tim->namaTim }}</td>
                     <td>
-                        <a class="btn btn-primary">Detail</a>
+                        <button type='button' class='btn btn-primary'>Detail</button>
                     </td>
                 </tr>
-                @empty
+                @endforeach
+                @if ($tims->isEmpty())
                 <tr>
                     <td colspan="4">Tidak ada tim yang tersedia.</td>
                 </tr>
-                @endforelse
+                @endif
             </tbody>
         </table>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
