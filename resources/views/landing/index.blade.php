298044cc9ff7f5e8d4afc77b7dd4d06605e399a4
iqbalrabani <iqbalrabani@student.ub.ac.id>
2023-06-27 10:51:14 +0700

    Delete icon

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,13 +5,9 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SoftLand Bootstrap Template - Index</title>
+  <title>LombaKuy</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="/images/favicon.png" rel="icon">
-  <link href="/images/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link
